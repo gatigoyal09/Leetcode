92c4c6eccb69f93f93d973d3756c52b8caec1a92
gatigoyal09 <gatigoyal09@gmail.com>
2026-07-04 14:02:23 +0530

    Time: 5 ms (59.87%), Space: 82.9 MB (46.35%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,10 +1,24 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        Map<Integer, Integer> mpp = new HashMap<>();
-        for(int el : nums){
-            mpp.put(el, mpp.getOrDefault(el,0)+1);
-            if(mpp.get(el)>1) return el;
+        // Map<Integer, Integer> mpp = new HashMap<>();
+        // for(int el : nums){
+        //     mpp.put(el, mpp.getOrDefault(el,0)+1);
+        //     if(mpp.get(el)>1) return el;
+        // }
+        // return 0;
+
+        // tortoise and hare methode
+        int slow = 0, fast = 0;
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+        // find entrance of cycle
+        slow = 0;
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return 0;
+        return slow;
     }
 }
