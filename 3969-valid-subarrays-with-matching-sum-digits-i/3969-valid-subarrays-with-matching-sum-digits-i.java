fd6d96c5f023349107b01959bf4a45a84cdc43a8
gatigoyal09 <gatigoyal09@gmail.com>
2026-07-13 11:14:59 +0530

    Time: 1167 ms (36.94%), Space: 46.7 MB (96.31%) - LeetHub

---
+++ 3969-valid-subarrays-with-matching-sum-digits-i/3969-valid-subarrays-with-matching-sum-digits-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countValidSubarrays(int[] nums, int x) {
+        int ans = 0;
+        
+        for(int i=0; i<nums.length; i++){
+            long pf = 0;
+            for(int j=i; j<nums.length; j++){
+                pf += nums[j];
+                long last = pf % 10;
+                long first = pf;
+                while(first > 9){
+                    first /= 10;
+                }
+                if(first == x && last == x) ans++;
+            }
+        }
+        return ans;
+    }
+}
